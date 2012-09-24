b1ebf06c083758478a153160e18e45c53cad60e5
Hugo Rodger-Brown <hugo@rodger-brown.com>
2012-09-24 19:27:33 +0100

    Added debugging for Heroku.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from os import environ
 import trello
 import hipchat
 import datetime
-from flask import Flask, session, escape
+import json
+from flask import Flask, session
 
 app = Flask(__name__)
 
@@ -10,22 +10,26 @@ app = Flask(__name__)
 @app.route('/<board>/<int:room>', methods=['GET'])
 def get_board_comments(board, room):
     """ Uses the trello lib to fetch latest comments from a board. """
-    if 'since' in session:
-        print 'fetching since from session'
-        since = session['since']
-    else:
-        print 'setting since to today()'
-        since = datetime.datetime(year=2012, month=1, day=1)
-    print 'since: {0}'.format(since)
-    for comment in trello.yield_latest_comments(board=board, since=since):
-        print str(comment)
-        if comment.date > since:
-            print 'updating since from {0} to {1}'.format(since, comment.date)
-            since = comment.date
-        hipchat.send_message(str(comment), room)
-    session['since'] = since
-    print escape(since)
-    return str(since)
+
+    try:
+        if 'since' in session:
+            print 'fetching since from session'
+            since = session['since']
+        else:
+            print 'setting since to today()'
+            since = datetime.datetime(year=2012, month=1, day=1)
+        print 'since: {0}'.format(since)
+        for comment in trello.yield_latest_comments(board=board, since=since):
+            print str(comment)
+            if comment.date > since:
+                print 'updating \'since\' from {0} to {1}'.format(since, comment.date)
+                since = comment.date
+            hipchat.send_message(str(comment), room)
+        session['since'] = since
+        return json.dumps({'since': since.isoformat()})
+    except:
+        from sys import exc_info
+        return json.dumps({'exception': str(exc_info()[1])})
 
 
 @app.route('/favicon.ico', methods=['GET'])
@@ -36,7 +40,6 @@ def get_favicon():
 if __name__ == '__main__':
     import settings  # import just to validate that settings exist
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(environ.get('PORT', 5000))
     app.debug = True
-    app.secret_key = 'something_really_really_secret'
-    app.run(host='0.0.0.0', port=port)
+    app.secret_key = '\xa35\xba\xf7\xb8\xc7\xbf\x97\xf4\xb5?\x99\xe5G\xc1\xbc\xdc\xb4\xaa\xe2\xc0\x0e\xde\x89'
+    app.run()
