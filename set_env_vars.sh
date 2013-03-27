6e4532e24620603364482c394196658c0faf4e40
Hugo Rodger-Brown <hugo@rodger-brown.com>
2013-03-27 15:59:25 +0000

    Removes set_env_vars scripts.
    These scripts aren't really required. Recommended method is to use
    Foreman and a .env file.

--- set_env_vars.sh
+++
@@ -1,8 +0,0 @@
-# HipChat settings for room notifications
-export HIPCHAT_API_TOKEN=<your_value_here>
-
-# Trello settings for reading event data
-export TRELLO_API_KEY=<your_value_here>
-export TRELLO_API_TOKEN=<your_value_here>
-
-export REDISTOGO_URL=<your_value_here>
